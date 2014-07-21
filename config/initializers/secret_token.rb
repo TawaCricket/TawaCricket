c726b7134f861c8cc13daf7cba479b47e8738440
ImaPotato <Scott.a@outlook.com>
2014-07-22 08:34:27 +1200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TawaCricketClub::Application.config.secret_key_base = 'fc1e2ee5a6f24149b242b90b13f3d058146aec7d1a3dc154c64ba444a2c229fb4063e2f68476f86e4aba08b66277ec922972f883ec7ea4105304f7dd02411ca7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TawaCricketClub::Application.config.session_store :cookie_store, key: '_TawaCricketClub_session'
