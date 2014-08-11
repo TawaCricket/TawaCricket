1243024f3f4dd319a00bf3aa4604c1620c9c1be1
Avikash-Mishra <avimish62@gmail.com>
2014-08-12 11:22:14 +1200

    erased code for imagick

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,5 @@
 class Image < ActiveRecord::Base    
- has_attached_file :pic, :styles => 
-          { :medium => "300x300>", :thumb => "100x100>" }
+ has_attached_file :pic
 
  validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
 end
