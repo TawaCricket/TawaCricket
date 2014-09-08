883ce6dd84b9a6c3fea7744ccfbfc32f0803a529
ImaPotato <Scott.a@outlook.com>
2014-09-09 10:33:49 +1200

    Added team management

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,9 +4,28 @@ class AdminController < ApplicationController
 		@juniors = Junior.all
 		@seniors = Senior.all
 		@teams = Team.all
+		print "\n"
+		print @teams
+		print "\n"
 	end
 
 	def save
+		@players = params[:players]
+		@players.each do |player|
+			print "\n"
+			print player
+			print "\n"
+			print player[1]
+			print "\n"
+			print player[1][:player]
+			print "\n"
+			print player[1][:team]
+			print "\n"
+			@some = Junior.find(player[1][:player])
+			@some.team_id = player[1][:team]
+			@some.save
+			print "\n"
+		end
 		
 		respond_to do |format|
     	    format.html { redirect_to root, notice: 'Succesfully saved changes.' }
